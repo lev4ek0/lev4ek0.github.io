e119aada022c36eba4b550f1fa86aba6932f7f39
Lev Belous <leva22.08.01@inbox.ru>
2021-11-29 13:50:43 +0300

    lab7

---
+++ js/nav.js
@@ -0,0 +1,21 @@
+let px480 = window.matchMedia( "(max-device-width: 480px)" );
+if (px480['matches']) {
+    document.getElementById('header').remove()
+    console.log('<')
+    let slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('menu'),
+        'padding': 256,
+        'tolerance': 70,
+        'easing': 'cubic-bezier(.32,2,.55,.27)'
+    });
+
+// Toggle button
+    document.querySelector('.toggle-button').addEventListener('click', function () {
+        slideout.toggle();
+    });
+} else {
+    console.log('>')
+    document.getElementById('mobile').remove()
+    document.getElementById('menu').remove()
+}
