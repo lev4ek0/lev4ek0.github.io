1368416e97aefecf0f73070c60dc83bc751f83f9
Lev Belous <leva22.08.01@inbox.ru>
2021-12-12 16:18:30 +0300

    some beatify

--- js/slider.js
+++ js/slider.js
@@ -7,5 +7,6 @@ window.onload = function () {
         draggable: false,
         wrapAround: true,
         accessibility: false,
+        arrowShape: 'M 0,50 L 60,00 L 50,30 L 80,30 L 80,70 L 50,70 L 60,100 Z' //m - move to, l - line to, z - end
     });
 }
